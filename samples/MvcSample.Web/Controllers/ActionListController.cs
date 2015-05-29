76e26342e44718ffce6a6c6ea917df0dd793d404
Ryan Nowak <nowakra@gmail.com>
2015-05-29 11:11:54 -0700

    Quick sample

---
+++ samples/MvcSample.Web/Controllers/ActionListController.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Routing.Template;
+
+namespace MvcSample.Web.Controllers
+{
+    public class ActionListController : Controller
+    {
+        private readonly IActionDescriptorsCollectionProvider _actionDescriptorsCollectionProvider;
+
+        public ActionListController(IActionDescriptorsCollectionProvider actionDescriptorCollectionProvider)
+        {
+            _actionDescriptorsCollectionProvider = actionDescriptorCollectionProvider;
+        }
+
+        public IActionResult Index()
+        {
+            var actions = _actionDescriptorsCollectionProvider.ActionDescriptors.Items;
+
+            var templateRoutes = new List<string>();
+            var routeCollection = ActionContext.RouteData.Routers[0] as RouteCollection;
+            if (routeCollection != null)
+            {
+                for (var i = 0; i < routeCollection.Count; i++)
+                {
+                    var route = routeCollection[i] as TemplateRoute;
+                    if (route != null)
+                    {
+                        templateRoutes.Add(route.RouteTemplate);
+                    }
+                }
+            }
+
+            var entries = new List<ActionListEntry>();
+            foreach (var action in actions)
+            {
+                var entry = new ActionListEntry(action);
+                entries.Add(entry);
+
+                if (action.AttributeRouteInfo?.Template == null)
+                {
+                    entry.Routes.AddRange(templateRoutes);
+                }
+                else
+                {
+                    entry.Routes.Add(action.AttributeRouteInfo.Template);
+                }
+            }
+
+            return View(entries);
+        }
+    }
+}
---
+++ samples/MvcSample.Web/Models/ActionListEntry.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace MvcSample.Web
+{
+    public class ActionListEntry
+    {
+        public ActionListEntry(ActionDescriptor action)
+        {
+            Action = action;
+
+            Routes = new List<string>();
+        }
+
+        public ActionDescriptor Action { get; }
+
+        public List<string> Routes { get; }
+    }
+}
